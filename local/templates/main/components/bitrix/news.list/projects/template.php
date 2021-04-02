1717aff70faa362eaffb7f9b9ef392c8efd52f8c
aavday <wrck7@yandex.ru>
2021-04-02 19:23:56 +0300

    Реализован вывод проектов на главной через компонент news.list

---
+++ local/templates/main/components/bitrix/news.list/projects/template.php
@@ -0,0 +1,25 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?if (!empty($arResult)):?>
+    <div class="owl-carousel owl-theme" id="project-slider">
+    <?foreach ($arResult["ITEMS"] as $arItem):?>
+        <div class="project__item">
+            <div class="project__item__img"><img src="<?=CFile::GetPath($arItem["PREVIEW_PICTURE"]["ID"])?>" alt=""/></div>
+            <div class="project__item__content"><img class="project__item__logo" src="<?=CFile::GetPath($arItem["DISPLAY_PROPERTIES"]["LOGO"]["VALUE"])?>" alt=""/>
+                <h4 class="project__item__name"><?=$arItem["NAME"]?></h4>
+                <ul class="project__item__list">
+                    <li><span class="project__item__list__preword">город</span>
+                    <p><?=$arItem["DISPLAY_PROPERTIES"]["CITY"]["VALUE"]?></p>
+                    </li>
+                    <li> <span class="project__item__list__preword">год</span>
+                    <p><?=$arItem["DISPLAY_PROPERTIES"]["YEAR"]["VALUE"]?></p>
+                    </li>
+                    <li> <span class="project__item__list__preword">материал</span>
+                    <p><?=$arItem["DISPLAY_PROPERTIES"]["MATERIAL"]["VALUE"]?></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    <?endforeach?>
+    </div>
+<?endif?>
