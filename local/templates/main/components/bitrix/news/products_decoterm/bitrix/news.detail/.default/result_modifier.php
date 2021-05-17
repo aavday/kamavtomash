9c7312e0b66c4ae860e73022d67230ada3c55c15
aavday <wrck7@yandex.ru>
2021-05-17 13:42:19 +0300

    сделал корректные ссылки на файлы прилагаемые к продукции

--- local/templates/main/components/bitrix/news/products_decoterm/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/main/components/bitrix/news/products_decoterm/bitrix/news.detail/.default/result_modifier.php
@@ -1,7 +1,7 @@
 <?
-$arResult['instruction'] = $arResult['DISPLAY_PROPERTIES']['INSTRUCTION']['VALUE'];
+$arResult['instruction'] = CFile::GetPath($arResult['DISPLAY_PROPERTIES']['INSTRUCTION']['VALUE']);
 $arResult['video'] = str_replace('/watch?v=', '/embed/', $arResult['DISPLAY_PROPERTIES']['VIDEO']['VALUE']);
-$arResult['certificates'] = $arResult['DISPLAY_PROPERTIES']['CERTIFICATES']['VALUE'];
+$arResult['certificates'] = CFile::GetPath($arResult['DISPLAY_PROPERTIES']['CERTIFICATES']['VALUE']);
 
 if ($arResult['video']) $GLOBALS["video"] = $arResult['video'];
 ?>
