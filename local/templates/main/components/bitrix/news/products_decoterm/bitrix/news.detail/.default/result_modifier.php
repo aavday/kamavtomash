c194e8d0ca409f133902e3ef9f5a182a8d0e4017
aavday <wrck7@yandex.ru>
2021-05-17 14:30:25 +0300

    сделал поддержку коротких ютубовских ссылок в карточке товара и подправил стили модалки для видео

--- local/templates/main/components/bitrix/news/products_decoterm/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/main/components/bitrix/news/products_decoterm/bitrix/news.detail/.default/result_modifier.php
@@ -1,7 +1,14 @@
 <?
 $arResult['instruction'] = CFile::GetPath($arResult['DISPLAY_PROPERTIES']['INSTRUCTION']['VALUE']);
-$arResult['video'] = str_replace('/watch?v=', '/embed/', $arResult['DISPLAY_PROPERTIES']['VIDEO']['VALUE']);
+$video = $arResult['DISPLAY_PROPERTIES']['VIDEO']['VALUE'];
 $arResult['certificates'] = CFile::GetPath($arResult['DISPLAY_PROPERTIES']['CERTIFICATES']['VALUE']);
 
-if ($arResult['video']) $GLOBALS["video"] = $arResult['video'];
+$video = str_replace('/watch?v=', '/embed/', $video);
+$video = str_replace('.be/', 'be.com/embed/', $video);
+
+if ($video)
+{
+    $GLOBALS["video"] = $video;
+    $arResult['video'] = $video;
+}
 ?>
